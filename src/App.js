e4d92b50380a9609d1fd192d53ec03ee5c262590
wenqianGU <guwenqian10@gmail.com>
2023-01-04 15:57:47 +1100

    categoryItem component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './categories.styles.scss'
 
+import CategoryItem from './components/category-item/category-item.component'
 const App = () => {
 
   const categories = [
@@ -32,17 +32,8 @@ const App = () => {
 
   return (
     <div className="categories-container">
-      {categories.map(({ title,id,imageUrl }) => (
-        <div key={id} className="category-container">
-          {/*<img /> */}
-          <div className="background-image" style={{
-             backgroundImage:`url(${imageUrl})`
-          }} />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
       ))}
     </div>
   );
