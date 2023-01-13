4e04977254999ed7daa496fb7b8309af4448e7bf
wenqianGU <guwenqian10@gmail.com>
2023-01-13 17:06:16 +1100

    test page

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -16,7 +16,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
 
 const provider = new GoogleAuthProvider();
 
